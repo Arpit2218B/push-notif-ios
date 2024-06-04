2b3158f622fb2183d18b41457c84a5a7586af8ac
Arpit Rathi <arpit97rathi@gmail.com>
2024-06-05 02:40:11 +0530

    feat: send notif in 5 secs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 import { displayNotificationDirectly, getPermission } from './utils';
 
 function App() {
+  const sendNotif = () => {
+    setInterval(displayNotificationDirectly, 5000);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={getPermission}>Get Permission</button>
-        <button onClick={displayNotificationDirectly}>Notify</button>
+        <button onClick={sendNotif}>Notify</button>
       </header>
     </div>
   );
 }
+// add
 
 export default App;
